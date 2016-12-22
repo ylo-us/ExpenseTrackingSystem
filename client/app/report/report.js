4d5e981d2ddc7258a9d120294471396e76350c44
kukulo <yalo1986@gmail.com>
2016-12-22 10:33:11 -0800

    Finished functions in report. 1. Allow user calculate weekly expenses. 2. Update transactions based on user's selected dates.

--- client/app/report/report.js
+++ client/app/report/report.js
@@ -1,30 +1,54 @@
 angular.module('report', [])
-.controller('ReportController', function($scope, $http) {
+.controller('ReportController', function($scope, $http, $filter) {
 	$scope.week = {
-    value: new Date()
+    value: new Date
   };
   $scope.date = {
-  	start: new Date(),
-  	end: new Date()
+  	start: new Date,
+  	end: new Date
   }
-  console.log('date: ', $scope.date);
+  $scope.weeklyExpense = 0;
 
 	$scope.expense = [];
+	$scope.updateExpense = [];
 	var url = 'http://localhost:8000/api/expense';
-	$scope.getExpense = function() {
+	
+	$scope.calWeekly = function() {
+		$scope.weeklyExpense = 0;
+		for (var i = 0; i < $scope.expense.length; i++) {
+			var weekOfTarget = $filter('date')($scope.expense[i].dateTime, 'w');
+			var weekOfCurrent = $filter('date')($scope.week.value, 'w');
+			if (weekOfCurrent === weekOfTarget) {
+				$scope.weeklyExpense += parseFloat($scope.expense[i].amount);
+			}
+		}
+	}
+
+	$scope.updateReport = function() {
+		$scope.updateExpense.length = [];
+		for (var i = 0; i < $scope.expense.length; i++) {
+			var currentDate = new Date($scope.expense[i].dateTime);
+			if (currentDate >= $scope.date.start 
+				&& currentDate <= $scope.date.end) {
+				$scope.updateExpense.push($scope.expense[i]);
+			}
+		}
+	}
+
+	var getExpense = function() {
 		$scope.expense.length = 0;
-		console.log('currentUser: ', window.currentUser);
 		$http({
 			method: 'GET',
 			url: url + '?id=' + window.currentUser,
 		}).then(function success(res) {
-			console.log($scope.week.value);
-			console.log('success res: ', res);
 			res.data.forEach(function(element) {
 				$scope.expense.push(element);
+				$scope.updateExpense.push(element);
 			})
+
 		}, function error(res) {
 			alert('Error happened!!');
 		})
 	}
+	getExpense();
 })
